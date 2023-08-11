0c268a561edcf5fec17d35b047488b124dac20e6
ivan odak <ivanodak7776@hotmail.com>
2023-08-11 21:40:50 +0200

    removed vue scaffold, added basic navbar and page outline

---
+++ src/components/Games/GameDialog.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const title = ref('');
+const description = ref('');
+</script>
+
+<template>
+  <div v-if="isOpen">
+    <div>
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Games/GamesList.vue
@@ -0,0 +1,21 @@
+<script setup>
+const items = [
+  { id: 1, name: 'Game 1' },
+  { id: 2, name: 'Game 2' },
+  { id: 3, name: 'Game 3' },
+  { id: 4, name: 'Game 4' },
+  { id: 5, name: 'Game 5' }
+];
+</script>
+
+<template>
+  <div>
+    <h2>Games</h2>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
