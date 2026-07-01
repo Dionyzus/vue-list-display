ea68330f6be28c3653ce9e4bc4994ca1ee17edab
ivanodak <ivan.odak@upf.edu>
2026-07-01 17:15:05 +0200

    Revert "#50 Hero banner presentation and main view composition (#63)"
    This reverts commit a184974b3d75d5aa861b1ef6e59222ea71d69497.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AppNavigation from './components/common/AppNavigation.vue';
 import AppPage from './components/common/AppPage.vue';
-import HeroBanner from './components/common/HeroBanner.vue';
 import GamesList from './components/Games/GamesList.vue';
 </script>
 
@@ -11,7 +10,6 @@ import GamesList from './components/Games/GamesList.vue';
       <AppNavigation />
     </template>
     <template v-slot:content>
-      <HeroBanner />
       <GamesList />
     </template>
   </AppPage>
